76e04398668f081bc19cadf5832baf9700fe64d2
sergiorbotelho <sergiorbotelho@gmail.com>
2023-06-14 13:59:14 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const email = document.querySelector(".mail")
+const password = document.querySelector(".password")
+const inputEmail = document.querySelector(".email")
+const inputPassword = document.querySelector(".pass")
+const iconMail = document.querySelector(".input #icon-mail")
+const iconEye = document.querySelector(".input #icon-eye")
+const iconPassword = document.querySelector(".input #icon-password")
+const html = document.querySelector("html")
+
+inputEmail.addEventListener("focus", () => {
+  if (html.classList.contains("ligth")) {
+    email.classList.add("ok")
+    iconMail.style.color = "#574c3a"
+  } else {
+    email.classList.add("ok")
+    iconMail.style.color = "#ffc632"
+  }
+})
+inputEmail.addEventListener("blur", () => {
+  email.classList.remove("ok")
+  iconMail.style.color = "#d4ccb6"
+})
+
+inputPassword.addEventListener("focus", () => {
+  if (html.classList.contains("ligth")) {
+    password.classList.add("ok")
+    iconPassword.style.color = "#574c3a"
+    iconEye.style.color = "#574c3a"
+  } else {
+    password.classList.add("ok")
+    iconPassword.style.color = "#ffc632"
+    iconEye.style.color = "#ffc632"
+  }
+})
+inputPassword.addEventListener("blur", () => {
+  password.classList.remove("ok")
+  iconPassword.style.color = "#d4ccb6"
+  iconEye.style.color = "#d4ccb6"
+})
